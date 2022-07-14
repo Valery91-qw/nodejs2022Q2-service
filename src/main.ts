fea44ac0336f32a0de571f3e05c94cc54d5fee84
Valery91-qw <vakriv91@gmail.com>
2022-07-14 15:10:28 +0300

    feat: add validation for id in albums controller

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { parse } from 'yaml';
 import { readFileSync } from 'fs';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   const file = readFileSync('./doc/api.yaml', 'utf-8');
   const document = parse(file);
   SwaggerModule.setup('doc', app, document);
